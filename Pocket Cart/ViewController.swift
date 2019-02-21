4c6eeee4998360e28292077474d2783e6f6b8001
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2019-02-21 15:34:47 +0530

    Conformed to MVC, Added app icon and images

---
+++ Pocket Cart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pocket Cart
+//
+//  Created by Hansa Anuradha on 2/21/19.
+//  Copyright © 2019 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
